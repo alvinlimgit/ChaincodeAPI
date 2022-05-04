9b6f8a3ea98fd18dc8d1bc524eca1836418d86a9
alvinlimgit <99379601+alvinlimgit@users.noreply.github.com>
2022-05-04 15:07:08 +0800

    Update smartcontract.go

--- asset-transfer-basic/chaincode-go/chaincode/smartcontract.go
+++ asset-transfer-basic/chaincode-go/chaincode/smartcontract.go
@@ -9,8 +9,8 @@ import (
         "strconv"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-        "github.com/hyperledger/fabric-chaincode-go/shim"
-        pb "github.com/hyperledger/fabric-protos-go/peer"
+    /*    "github.com/hyperledger/fabric-chaincode-go/shim"
+        pb "github.com/hyperledger/fabric-protos-go/peer" */
 )
 
 // SmartContract provides functions for managing an Asset
@@ -26,7 +26,7 @@ type Asset struct {
 	Owner          string `json:"owner"`
 	AppraisedValue int    `json:"appraisedValue"`
 }
-
+/*
 // Alvin addition. To use getHistoryForKey() METHOD, in ChaincodeStubInterface INTERFACE, in Shim PACKAGE. Function needs to exist in SMART CONTRACT.
 // /fabric-samples/asset-transfer-basic/chaincode-go/assetTransfer.go is the executable code, which imports chaincode (this file) & only runs main function
 // to initialize. Modular. Import other stuff from other repo, keep main executable code clean.
@@ -94,11 +94,11 @@ func (s *SmartContract) AlvinGet(stub shim.ChaincodeStubInterface, args []string
 
         return shim.Success(buffer.Bytes())
 }
-
+*/
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	assets := []Asset{
-		{ID: "asset1", Color: "bablabla", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
+		{ID: "asset1", Color: "baba", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
 		{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
 		{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
 		{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
